3af143f72c92dae41dc90af9fcdae97139df5cd0
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2022-11-30 21:28:02 -0300

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -47,8 +47,11 @@ class TitleState extends MusicBeatState
 
 	override public function create():Void
 	{
+		#if android
+		FlxG.android.preventDefaultKeys = [BACK];
+		#end
 		
-		#if sys
+		#if !android // Fuedase?
 		if (!sys.FileSystem.exists(Sys.getCwd() + "/assets/replays"))
 			sys.FileSystem.createDirectory(Sys.getCwd() + "/assets/replays");
 		#end 
